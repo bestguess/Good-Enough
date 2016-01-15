e9ce424f1c92c0321bf29f56449bb31c8cf57ff7
thecitywelivein <thecitywelivein@gmail.com>
2016-01-14 19:56:01 -0600

    Linked Server Routes

--- server/messages/messagesRoutes.js
+++ server/messages/messagesRoutes.js
@@ -1,5 +1,8 @@
 var messagesController = require('./messages/messagesController');
 
 module.exports = function(app) {
-
+  app.post('/new', messageController.startConvo);
+  app.get('/list', messageController.getConvo);
+  app.post('/reply', messageController.reply);
+  app.get('/messages', messageController.getMessages)
 };
