4f7451b79cb44a93fbab9579d9811858abb7665b
thecitywelivein <thecitywelivein@gmail.com>
2016-01-15 15:31:21 -0600

    Finished Messaging API

--- server/messages/messagesRoutes.js
+++ server/messages/messagesRoutes.js
@@ -2,7 +2,7 @@ var messagesController = require('./messagesController.js');
 
 module.exports = function(app,express) {
   app.post('/new', messagesController.startConvo);
-  app.get('/list', messagesController.getConvo);
+  app.post('/list', messagesController.getConvo);
   app.post('/reply', messagesController.reply);
-  app.get('/messages', messagesController.getMessages)
+  app.post('/messages', messagesController.getMessages)
 };
