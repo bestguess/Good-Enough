c2e42f5e6390b0305ea75e368037fcca517d4344
Hank Couture <hankcouture@gmail.com>
2016-01-15 16:24:26 -0600

    now storing answers in state

--- client/app/actions/questions.js
+++ client/app/actions/questions.js
@@ -1,6 +1,5 @@
 import * as types from '../constants/ActionTypes'
 
-export function answerQuestion(answer) {
-	if (answer !== undefined) answer = "MakerSquare"
-	return { type: types.ANSWER_QUESTION, answer }
+export function answerQuestion(id, answer) {
+	return { type: types.ANSWER_QUESTION, id, answer }
 }
