a89b57a5a08ccb6df2f88edada9f1bd24164e8a8
thecitywelivein <thecitywelivein@gmail.com>
2016-01-15 14:03:21 -0600

    Message functionality

--- server/messages/messagesController.js
+++ server/messages/messagesController.js
@@ -4,15 +4,15 @@ var Messages = db.Messages;
 
 module.exports = {
   startConvo : function(req, res, next){
-    // var to = req.body.to;
-    // var from = req.body.from;
+    var to = req.body.to;
+    var from = req.body.from;
   },
   getConvo : function(req, res, next){
 
   },
   getMessages : function(req, res, next){
-    // console.log("hi");
-    // res.status(200).send("Hi");
+    console.log("hi");
+    res.status(200).send("Hi");
   },
   reply : function(req, res, next){
 
