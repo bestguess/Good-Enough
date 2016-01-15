77eb4dcacd8685a118eb6e15310f123d486c18da
ivanmora <moraivan1@gmail.com>
2016-01-15 10:14:03 -0600

    Added user routes

--- server/users/usersController.js
+++ server/users/usersController.js
@@ -1,15 +1,27 @@
-var db = require('./db_config.js');
+var db = require('../db_config.js');
 var mongoose = require('mongoose');
 var User = db.Users;
 
 module.exports = {
 
-  createUser: function(req, res, nest){
+  getEmails: function(res){
+    User.find({}, function(err, people){
+      if(err) throw err;
+
+      var emails = [];
+      for(var i = 0; i < people.length; i++){
+        emails.push(people[i].email);
+      }
+      res.status(302).send(emails);
+    })
+  },
+
+  signUp: function(req, res){
     var user = req.body;
     // To be populated and submitted as a new user
     var userObject = {};
     // Required fields with which to create user
-    var properties = [firstName, lastName, email, password, age, gender, interests, type, personality, picture, places];
+    var properties = [firstName, lastName, email, password, age, gender, interests, type, personality, picture, places, matches];
     var failings = [];
     var failed = false;
 
@@ -27,14 +39,29 @@ module.exports = {
 
     var newUser = User(userObject);
     newUser.save(function(err, user){
-      if (err) throw err;
+      if(err) throw err;
       // If no save error then send the user's new id
       res.status(201).send(user._id);
     })
   },
 
-  getUsernames: function(){
-    User.find({}, function(err, people))
+  signIn: function(req, res){
+    var user = req.data;
+    // Requires that a user provides an email and password
+    if(!user.email || !user.password) res.status(400).send();
+    User.findOne({email: user.email, password: user.password}, function(err, user){
+      if(err) throw err;
+      var userInfo = {}
+      userInfo.firstName = user.firstName;
+      userInfo.lastName = user.lastName;
+      userInfo.picture = user.picture;
+      userInfo.meet = user.meet;
+
+      // Send back info needed for home page
+      res.status(200).send(userInfo);
+    })
   }
+
+  // ToDo: changePicture function
 
 };
--- server/users/usersRoutes.js
+++ server/users/usersRoutes.js
@@ -1,5 +1,7 @@
 var usersController = require('./usersController.js');
 
 module.exports = function(app) {
-
+  app.get('/emails', usersController.getEmails);
+  app.post('/signUp', usersController.signUp);
+  app.get('/signIn', usersController.signIn);
 };
