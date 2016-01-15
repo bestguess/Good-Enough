1ca92b3817ee1a9eed0bede13703c126da044ef5
ivanmora <moraivan1@gmail.com>
2016-01-15 15:37:18 -0600

    signUp and getEmails are functional and routed

--- server/users/usersRoutes.js
+++ server/users/usersRoutes.js
@@ -2,6 +2,6 @@ var usersController = require('./usersController.js');
 
 module.exports = function(app) {
   app.get('/emails', usersController.getEmails);
-  app.post('/signUp', usersController.signUp);
-  app.get('/signIn', usersController.signIn);
+  app.post('/signup', usersController.signUp);
+  app.get('/signin', usersController.signIn);
 };
