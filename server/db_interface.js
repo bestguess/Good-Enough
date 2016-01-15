9d4f852f8ea215dd594ae0d2eeffa670775c842d
thecitywelivein <thecitywelivein@gmail.com>
2016-01-14 19:56:01 -0600

    Linked Server Routes

--- server/db_interface.js
+++ server/db_interface.js
@@ -1,16 +1,16 @@
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
 
 module.exports = function(app, express){
-  app.use(morgain('dev'));
+  app.use(morgan('dev'));
   app.use(bodyParser.json());
   app.use(cookieParser());
   app.use(express.static('../client/index.html'));
-
   var usersRouter = express.Router();
   var messagesRouter = express.Router();
-
-  require('./users/userRoutes.js');
+  console.log("his");
+  require('./users/usersRoutes.js');
   require('./messages/messagesRoutes.js');
 
   app.use('/app/users')(userRoutes);
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 4000;
 
 
 // Server configuration in db_interface
-require('./db_interface');
+require('./db_interface.js')(app, express);
 
 app.listen(port, function(){
   console.log('Listening on port %s', port);
