e9e606089d9f3067550d27f20d417762b92cc1c0
thecitywelivein <thecitywelivein@gmail.com>
2016-01-15 11:01:49 -0600

    fixed npm start

--- server/messages/messagesRoutes.js
+++ server/messages/messagesRoutes.js
@@ -1,8 +1,8 @@
 var messagesController = require('./messagesController.js');
 
 module.exports = function(app) {
-  // app.post('/new', messagesController.startConvo);
-  // app.get('/list', messagesController.getConvo);
-  // app.post('/reply', messagesController.reply);
-  // app.get('/messages', messagesController.getMessages)
+  app.post('/new', messagesController.startConvo);
+  app.get('/list', messagesController.getConvo);
+  app.post('/reply', messagesController.reply);
+  app.get('/messages', messagesController.getMessages)
 };
