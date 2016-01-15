e1c0676595b485cefce999319019279c53fa01ba
ivanmora <moraivan1@gmail.com>
2016-01-15 15:37:18 -0600

    signUp and getEmails are functional and routed

--- server/users/usersController.js
+++ server/users/usersController.js
@@ -7,45 +7,59 @@ console.log('in usersController')
 module.exports = {
 
   getEmails: function(req, res){
-    User.find({}, function(err, people){
-      if(err) throw err;
-
-      var emails = [];
-      for(var i = 0; i < people.length; i++){
-        emails.push(people[i].email);
+    User.find({}, 'email', function(err, emails){
+      if(err){
+        res.status(404).send(err);
+      }else{
+        // var emails = [];
+        // for(var i = 0; i < people.length; i++){
+        //   emails.push(people[i].email);
+        // }
+        res.status(302).send(emails);
       }
-      res.status(302).send(emails);
     })
 
   },
 
-  signUp: function(req, res){
+  signUp: function(req, res, next){
+    console.log(req.body);
     var user = req.body;
     // To be populated and submitted as a new user
     var userObject = {};
     // Required fields with which to create user
-    var properties = [firstName, lastName, email, password, age, gender, interests, type, personality, picture, places, matches];
+    var properties = {firstName:'firstName', lastName:'lastName', email:'email', password:'password', age:'age', gender:'gender', 
+        interests:'interests', type:'type', personality:'personality', picture:'picture', places:'places', matches:'matches'};
     var failings = [];
     var failed = false;
 
     for(var key in properties){
       if(!user[key]){
-        failings.push(key);
+        failings.push(properties[key]);
         failed = true;
       }else{
+        if(key === 'interests' || key === 'personality') user[key] = JSON.stringify(user[key]);
         userObject[key] = user[key];
       }
     }
     // If any of the fields are not submitted then send 400 
     // and list of missing fields
-    if(failed) res.status(400).send(failings);
+    if(failed){
+      res.status(400).send(failings);
+      next();
+    }else{
+      var newUser = User(userObject);
+      newUser.save(function(err, user){
+        if(err){
+          res.status(500).send(err);
+          next();
+        }else{
+          // If no save error then send the user's new id
+          res.status(201).send(user._id);
+          next();
+        }
+      })
+    }
 
-    var newUser = User(userObject);
-    newUser.save(function(err, user){
-      if(err) throw err;
-      // If no save error then send the user's new id
-      res.status(201).send(user._id);
-    })
   },
 
   signIn: function(req, res){
