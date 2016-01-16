1af1000a33c50e71a395a5f6fbb86af78e772455
thecitywelivein <thecitywelivein@gmail.com>
2016-01-16 12:01:06 -0600

    Wrote Basic Mocha Test

--- server/test/serverAPI.js
+++ server/test/serverAPI.js
@@ -1,11 +1,13 @@
-var port = 4000;
+var superagent = require('superagent');
+var expect = require('expect');
 
 describe('homepage', function(){
   it('should respond to GET',function(done){
     superagent
-      .get('http://localhost:'+ port)
+      .get('http://localhost:4000')
       .end(function(res){
         expect(res.status).to.equal(200);
         done();
     })
   })
+})
