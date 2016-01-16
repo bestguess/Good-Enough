1b657c4019ec440387a3fe664bd22dc440d3e409
thecitywelivein <thecitywelivein@gmail.com>
2016-01-15 19:53:44 -0600

    added backend photobooth functionality

--- server/users/usersController.js
+++ server/users/usersController.js
@@ -1,7 +1,6 @@
 var db = require('../db_config.js');
 var mongoose = require('mongoose');
 var User = db.Users;
-var fs = require('fs');
 var photo = require('../helpers/picture_conversion.js');
 var match = require('../helpers/matching_algo.js');
 
@@ -45,6 +44,7 @@ module.exports = {
       res.status(400).send(failings);
       next();
     }else{
+      userObject.picture = photo.convert(userObject.picture, userObject.email);
       var newUser = User(userObject);
       newUser.save(function(err, user){
         if(err){
