22e96c91dafa79d731949afe8e06aebe4972c2a4
Hank Couture <hankcouture@gmail.com>
2016-01-16 10:45:26 -0600

    still working on api middleware

--- client/app/reducers/index.js
+++ client/app/reducers/index.js
@@ -1,8 +1,35 @@
 import { combineReducers } from 'redux'
+import * as ActionTypes from '../constants/ActionTypes'
 import questions from './questions'
 
+
+// Updates an entity cache in response to any action with response.entities.
+function entities(state = { users: {}, repos: {} }, action) {
+  if (action.response && action.response.entities) {
+    return merge({}, state, action.response.entities)
+  }
+
+  return state
+}
+
+// Updates error message to notify about the failed fetches.
+function errorMessage(state = null, action) {
+  const { type, error } = action
+
+  if (type === ActionTypes.RESET_ERROR_MESSAGE) {
+    return null
+  } else if (error) {
+    return action.error
+  }
+
+  return state
+}
+
+
 const rootReducer = combineReducers({
-  questions
+	entities,
+	questions,
+	errorMessage
 })
 
 export default rootReducer
