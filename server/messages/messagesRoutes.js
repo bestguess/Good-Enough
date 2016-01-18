023d1e66272e1a540ca0f6fdd68627073521b64d
thecitywelivein <thecitywelivein@gmail.com>
2016-01-18 14:25:49 -0600

    bcrypt passwords

--- server/messages/messagesRoutes.js
+++ server/messages/messagesRoutes.js
@@ -3,9 +3,9 @@ var helpers = require('../helpers/helpers.js');
 
 module.exports = function(app, passport) {
   app.post('/new', messagesController.startConvo);
-  //app.post('/list', helpers.isLoggedIn, messagesController.getConvo);
-  app.post('/list', messagesController.getConvo);
+  app.post('/list', helpers.isLoggedIn, messagesController.getConvo);
+  // app.post('/list', messagesController.getConvo);
   app.post('/reply', messagesController.reply);
-  //app.post('/messages', helpers.isLoggedIn, messagesController.getMessages);
-  app.post('/messages', messagesController.getMessages);
+  app.post('/messages', helpers.isLoggedIn, messagesController.getMessages);
+  // app.post('/messages', messagesController.getMessages);
 };
