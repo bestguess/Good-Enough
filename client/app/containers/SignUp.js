6a3c3a218e492e68a4d3016bd7da5a10c4840b6b
Hank Couture <hankcouture@gmail.com>
2016-01-18 17:03:14 -0600

    cleaned up Redux code and changed file structure

--- client/app/containers/SignUp.js
+++ client/app/containers/SignUp.js
@@ -5,11 +5,11 @@ import QuestionForm from '../components/questionForm'
 import * as QuestionActions from '../actions/signup'
 const { Link } = require('react-router');
 
-class App extends Component {
+class SignUp extends Component {
   render() {
     const { state, actions } = this.props
     var h1Content;
-    if (this.props.state.questions.viewData.signup.stage0) {
+    if (this.props.state.signup.viewData.signup.stage0) {
       h1Content = <img src="../client/img/logo.png" style={{width: 450 + "px"}}/>
     } else {
       h1Content = "Good Enough"
@@ -23,7 +23,7 @@ class App extends Component {
   }
 };
 
-App.propTypes = {
+SignUp.propTypes = {
   state: PropTypes.object.isRequired,
   actions: PropTypes.object.isRequired
 }
@@ -43,5 +43,5 @@ function mapDispatchToProps(dispatch) {
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(App)
+)(SignUp)
 
