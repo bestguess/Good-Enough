5110dd3245b03b496c1ac42db3b16459c4888bfb
Paolinni Velazquez <Paolinni@Paolinnis-MacBook-Pro.local>
2016-01-18 19:55:54 -0600

    added seperate logIn action/working on logIn reducer (fetch)

--- client/app/components/LogInForm.js
+++ client/app/components/LogInForm.js
@@ -3,14 +3,15 @@ import React, { Component, PropTypes } from 'react'
 class LogInForm extends Component {
 
   handleKeyUp(input) {
-    this.props.actions.saveInput(input, this.refs[input].value)
+    this.props.actions.saveLogInInput(input, this.refs[input].value)
   }
 
   render() {
-    var formButton = <button onClick={this.props.actions.submitSurvey} className="question-form-button">Submit</button>
+    var formButton = <button onClick={this.props.actions.logIn} className="question-form-button">Submit</button>
 
     return (
       <div>
+        <h1 className="logo">Login</h1>
         <div className="user-info-form">
           <input placeholder="Enter Email" ref="email" onKeyUp={() => this.handleKeyUp('email')} />
           <input placeholder="Enter Password" ref="password" onKeyUp={() => this.handleKeyUp('password')} />
