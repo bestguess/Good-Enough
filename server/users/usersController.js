21553aba5589c7f4e645a2870e868fc11486554e
thecitywelivein <thecitywelivein@gmail.com>
2016-01-19 10:15:50 -0600

    Finished Algo 1st Draft

--- server/users/usersController.js
+++ server/users/usersController.js
@@ -18,48 +18,52 @@ module.exports = {
   signUp: function(req, res, next){
     var user = req.body;
     
-    // To be populated and submitted as a new user
-    var userObject = {};
-    // Required fields with which to create user
-    var properties = {firstName:'firstName', lastName:'lastName', email:'email', password:'password', age:'age', gender:'gender', 
-        interests:'interests', type:'type', personality:'personality', picture:'picture', places:'places', matches:'matches'};
-    var failings = [];
-    var failed = false;
+    match.user(user, matchMe);
 
-    for(var key in properties){
-      if(!user[key]){
-        failings.push(properties[key]);
-        failed = true;
+    function matchMe(data){
+      user.matches = data;
+      // To be populated and submitted as a new user
+      var userObject = {};
+      // Required fields with which to create user
+      var properties = {firstName:'firstName', lastName:'lastName', email:'email', password:'password', age:'age', gender:'gender', 
+          interests:'interests', type:'type', personality:'personality', picture:'picture', places:'places', matches:'matches'};
+      var failings = [];
+      var failed = false;
+
+      for(var key in properties){
+        if(!user[key]){
+          failings.push(properties[key]);
+          failed = true;
+        }else{
+          if(key === 'interests' || key === 'personality') user[key] = JSON.stringify(user[key]);
+          userObject[key] = user[key];
+        }
+      }
+
+      // If any of the fields are not submitted then send 400 
+      // and list of missing fields
+      if(failed){
+        res.status(400).send(JSON.parse(failings));
+        next();
       }else{
-        if(key === 'interests' || key === 'personality') user[key] = JSON.stringify(user[key]);
-        userObject[key] = user[key];
+        userObject.picture = photo.convertPhoto(userObject.picture, userObject.email);
+        
+        bcrypt.hash(userObject.password, userObject.password, function(err, hash) {
+          userObject.password = hash;
+          var newUser = User(userObject);
+          newUser.save(function(err, user){
+            if(err){
+              res.status(500).send(err);
+              next();
+            }else{
+              // If no save error then send the user's new id
+              res.status(201).send(user._id);
+              next();
+            }
+          });
+        });
       }
     }
-
-    // If any of the fields are not submitted then send 400 
-    // and list of missing fields
-    if(failed){
-      res.status(400).send(JSON.parse(failings));
-      next();
-    }else{
-      userObject.picture = photo.convertPhoto(userObject.picture, userObject.email);
-      
-      bcrypt.hash(userObject.password, userObject.password, function(err, hash) {
-        userObject.password = hash;
-        var newUser = User(userObject);
-        newUser.save(function(err, user){
-          if(err){
-            res.status(500).send(err);
-            next();
-          }else{
-            // If no save error then send the user's new id
-            res.status(201).send(user._id);
-            next();
-          }
-        });
-      });
-    }
-
   },
 
   signIn: function(req, res){
