ae68edab7ae315777c40aa221058b0f6b32bc12c
Paolinni Velazquez <Paolinni@Paolinnis-MacBook-Pro.local>
2016-01-20 16:52:30 -0600

    added thunk-middleware to handle async functions for login

--- client/app/actions/profile.js
+++ client/app/actions/profile.js
@@ -1,9 +1,39 @@
 import * as types from '../constants/Profile_ActionTypes'
 
-export function profile() {
-	return { type: types.PROFILE }
+export function optimisticProfile(newData) {
+  console.log('newData: ', newData);
+	return { type: types.PROFILE, newData }
 }
 
 export function logout() {
-	return { type: types.LOGOUT }	
+	return { type: types.LOGOUT }
+}
+
+export function profile() {
+  return function (dispatch, getState) {
+    // Gather User ID and Session Token from Local Storage
+    var userData = window.localStorage.getItem('GoodEnough')
+    fetch('http://localhost:4000/app/users/info', {
+        method: 'post',
+        headers: {
+          'mode': 'no-cors',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(JSON.parse(userData))
+      })
+      .then(res => {
+        if (res.status >= 200 && res.status < 300) {
+          res.json().then(data => {console.log('Server Response: ', data);
+            dispatch(optimisticProfile(data));
+          });
+        } else {
+          const error = new Error(res.statusText);
+          error.res = res;
+          throw error;
+        }
+      })
+      .catch(error => { console.log('request failed', error)});
+  }
+  return null;
 }
--- client/app/reducers/login.js
+++ client/app/reducers/login.js
@@ -17,32 +17,8 @@ export default function LogIn(state = initialState, action) {
       state.userData[action.input] = action.value
       return state
     case LOG_IN:
-      console.log('state yo', state);
-
-      fetch('http://localhost:4000/app/users/signin', {
-        method: 'post',
-        headers: {
-          'mode': 'no-cors',
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(state.userData)
-      })
-      .then(res => {
-        console.log('res: ', res)
-        if (res.status >= 200 && res.status < 400) {
-          console.log('original: ', res)
-          res.json().then(data => {console.log('jsoned data: ', data);
-          window.localStorage.setItem('GoodEnough', JSON.stringify(data))});
-        } else {
-          const error = new Error(res.statusText);
-          error.res = res;
-          throw error;
-        }
-      })
-      .catch(error => { console.log('request failed', error)});
-      console.log('submitting survey')
-      return state
+      var newState = Object.assign({}, state)
+      return newState
     default:
       console.log('hit default case: returning state')
     	return state
