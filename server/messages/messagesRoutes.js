597bba4fb86622116a37949db70d31e88227c211
thecitywelivein <thecitywelivein@gmail.com>
2016-01-20 10:57:41 -0600

    Fixed Mocha Tests to work with tokens

--- server/messages/messagesRoutes.js
+++ server/messages/messagesRoutes.js
@@ -1,9 +1,9 @@
 var messagesController = require('./messagesController.js');
 var helpers = require('../helpers/helpers.js');
 
-module.exports = function(app, passport) {
+module.exports = function(app) {
   app.post('/new', messagesController.startConvo);
-  app.post('/list', helpers.isLoggedIn, messagesController.getConvo);
+  app.post('/list', messagesController.getConvo);
   app.post('/reply', messagesController.reply);
-  app.post('/messages', helpers.isLoggedIn, messagesController.getMessages);
+  app.post('/messages', messagesController.getMessages);
 };
