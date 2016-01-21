be4ffa023571701f27ad609dbbe526b6ea0a4fb6
Hank Couture <hankcouture@gmail.com>
2016-01-21 15:30:54 -0600

    now making match server request on match page

--- client/app/containers/Match.js
+++ client/app/containers/Match.js
@@ -4,6 +4,37 @@ import { connect } from 'react-redux'
 import * as MatchActions from '../actions/match'
 import PrivateNav from '../components/PrivateNav'
 
+function status(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return Promise.resolve(response)
+  } else {
+    return Promise.reject(new Error(response.statusText))
+  }
+}
+
+function json(response) { return response.json() }
+
+function getMatchInfo(props) {
+  var requestData = JSON.parse(window.localStorage.getItem('GoodEnough'))
+  requestData.match_id = props.state.routing.location.pathname.substring(1)
+  console.log('requestData: ', requestData)
+  fetch('http://localhost:4000/app/matches/match', {
+          method: 'POST',
+          headers: { 'mode': 'no-cors', 'Accept': 'application/json', 'Content-Type': 'application/json' },
+          body: JSON.stringify(requestData)
+        })
+    .then(status)
+    .then(json)
+    .then(function(data) {
+      console.log('Request succeeded with JSON response', data);
+      // props.actions.profile(data)
+    }).catch(function(error) {
+      console.log('Request failed', error);
+    });
+}
+
+
+
 class MatchPicture extends Component {
   render() {
     return (
@@ -40,11 +71,11 @@ class MatchMessageImage extends Component {
   render() {
     return (
       <div className="match-conversation-image">
-        <img src={this.props.state.profile.data.picture}/>
       </div>
     );
   }
 }
+// <img src={this.props.state.profile.data.picture}/>
 
 class MatchMessage extends Component {
   render() {
@@ -81,7 +112,13 @@ class MatchConversation extends Component {
 }
 
 class Match extends Component {
+
+  componentWillMount() {
+    getMatchInfo(this.props)
+  }
+
   render() {
+    console.log(this.props.state.routing.location.pathname.substring(1))
     return (
       <div>
         <PrivateNav state={this.props.state} actions={this.props.actions} />
@@ -99,7 +136,7 @@ Match.PropTypes = {
 
 function mapStateToProps(state) {
   return {
-    state: state
+    state: state,
   }
 }
 
