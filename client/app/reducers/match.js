653b62692f3f72ea37da8f90bb3fa8f0ff58fb76
Hank Couture <hankcouture@gmail.com>
2016-01-21 16:46:46 -0600

    enabled logout on match page

--- client/app/reducers/match.js
+++ client/app/reducers/match.js
@@ -1,4 +1,4 @@
-import { SAVE_MATCH_DATA, SAVE_INPUT, SEND_MESSAGE } from '../constants/Match_ActionTypes'
+import { SAVE_MATCH_DATA, SAVE_INPUT, SEND_MESSAGE, LOGOUT } from '../constants/Match_ActionTypes'
 
 const initialState = {
 
@@ -22,6 +22,30 @@ export default function Match(state = initialState, action) {
       var newState = Object.assign({}, state)
     	console.log('sending message...')
     	return newState
+
+
+    case LOGOUT:
+      var newState = Object.assign({}, state)
+
+      // Gather User ID and Session Token from Local Storage
+      var userData = window.localStorage.getItem('GoodEnough')
+      // Make server request to delete token storage on server side
+      fetch('http://localhost:4000/app/users/logout', {
+        method: 'post',
+        headers: {
+          'mode': 'no-cors',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(JSON.parse(userData))
+      })
+
+      // Remove local storage ID and Token
+      window.localStorage.removeItem('GoodEnough');
+
+      // Reset Initial Profile State (for if a different user logs on right after logout)
+      newState = {};
+      return newState
 
 
     default:
