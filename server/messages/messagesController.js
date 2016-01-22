d2bbe20b8bebfed33dfffc80c497637c882a5a49
Hank Couture <hankcouture@gmail.com>
2016-01-21 19:42:18 -0600

    now displaying messages when we have them

--- server/messages/messagesController.js
+++ server/messages/messagesController.js
@@ -5,6 +5,7 @@ var Messages = db.Messages;
 module.exports = {
 
   send : function(req, res, next){
+    console.log(req.body)
     var date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
     var text = {
       user : req.body.from,
@@ -17,7 +18,7 @@ module.exports = {
         res.status(500).send(err);
         return next();
       }
-      if(!convo){
+      if(convo === null){
         var newConvo = new Messages;
         newConvo.users.push( req.body.to , req.body.from );
         newConvo.messages.push(text);
@@ -26,25 +27,26 @@ module.exports = {
         else res.status(201).send(text);
         return next();
         });
-      }  
-      Messages.update({_id: convo._id}, {
-        $push: { messages : text}
-      }, function(err) { 
-        if(err) res.status(400).send(err);
-        else {
-          Messages.findOne({users: {$all:[req.body.to, req.body.from]}}, function(err, convo){
-            if(err){
-              res.status(500).send(err);
-              return next();
-            }
-            if(!convo){
-              res.status(404).send("Conversation not found")
-              return next();
-            }
-            res.status(200).send(convo);
-         });
-        }
-      });
+      } else {
+        Messages.update({_id: convo._id}, {
+          $push: { messages : text}
+        }, function(err) { 
+          if(err) res.status(400).send(err);
+          else {
+            Messages.findOne({users: {$all:[req.body.to, req.body.from]}}, function(err, convo){
+              if(err){
+                res.status(500).send(err);
+                return next();
+              }
+              if(!convo){
+                res.status(404).send("Conversation not found")
+                return next();
+              }
+              res.status(200).send(convo);
+           });
+          }
+        }); 
+      }
     });
   },
 
