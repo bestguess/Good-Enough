2c4dcc84f421779b8cc21a444ef1dc995ce24c2d
Hank Couture <hankcouture@gmail.com>
2016-01-22 16:00:24 -0600

    cleaned up front end bugs

--- client/app/constants/Match_ActionTypes.js
+++ client/app/constants/Match_ActionTypes.js
@@ -2,3 +2,5 @@ export const SAVE_MATCH_DATA = 'SAVE_MATCH_DATA'
 export const SEND_MESSAGE = 'SEND_MESSAGE'
 export const SAVE_INPUT = 'SAVE_INPUT'
 export const LOGOUT = 'LOGOUT'
+export const CLEAR_CURRENT_MATCH_DATA = 'CLEAR_CURRENT_MATCH_DATA'
+
