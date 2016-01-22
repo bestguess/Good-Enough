406875f82f46219cfda2b69bc9e424916a6fd09a
ivanmora <moraivan1@gmail.com>
2016-01-22 10:57:58 -0600

    refactored messagesController

--- server/matches/matchesController.js
+++ server/matches/matchesController.js
@@ -1,6 +1,7 @@
 var db = require('../db_config.js');
 var mongoose = require('mongoose');
 var User = db.Users;
+var Messages = db.Messages;
 var helpers = require("../helpers/helpers.js");
 
 module.exports = {
@@ -15,18 +16,32 @@ module.exports = {
       if(!match){
         res.status(404).send();
         return next();
-      }
+      }else{
       var properties = new helpers.UserData;
       var matchObject = {}
       for(var key in properties){
         if(
           key !== 'password' &&
           key !== 'matches' &&
-          key !== 'conversations' &&
+          key !== 'conversati ons' &&
           key !== 'meet'
           ) matchObject[key] = match[key];
       }
+      console.log('user', req.body._id, 'match', match._id);
+      Messages.findOne({users: {$all:[req.body._id, match._id]}}, function(err, convo){
+            if(err){
+              res.status(500).send(err);
+              return next();
+            }
+            if(convo){
+              matchObject.conversations = convo;
+              console.log('matchObject', matchObject)
+              res.status(200).send(matchObject);
+              return next();
+            }
+         });
       res.status(200).send(matchObject);
+    }
     });
   }
 
