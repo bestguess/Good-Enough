ca70e686b3ebf501c2b293f44a32ca4124338dc4
Hank Couture <hankcouture@gmail.com>
2016-01-22 15:23:46 -0600

    code clean up

--- client/app/containers/SignUp.js
+++ client/app/containers/SignUp.js
@@ -14,7 +14,6 @@ class SignUp extends Component {
 
   render() {
     const { state, actions } = this.props
-    console.log(this)
     var h1Content;
     if (this.props.state.signup.viewData.signup.stage0) {
       h1Content = <div className="logo-img"><img src="../client/img/logo.png"/></div>
