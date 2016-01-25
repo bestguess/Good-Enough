50b365c222f05cf1ea076ede4a08d3c7f19c599a
thecitywelivein <thecitywelivein@gmail.com>
2016-01-25 15:47:30 -0600

    changed matches from array to array of objects

--- server/db_config.js
+++ server/db_config.js
@@ -24,9 +24,7 @@ var usersSchema = new Schema({
   picture: String,
   places: [String],
   matches: [Schema.Types.Mixed],
-  questions: [Schema.Types.Mixed],
-  conversations: [Schema.Types.ObjectId],
-  meet: [String]
+  questions: [Schema.Types.Mixed]
 });
 
 var messagesSchema = new Schema({
