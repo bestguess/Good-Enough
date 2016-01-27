6be5effe715878d8ca2dd60dac2859625a61c89e
thecitywelivein <thecitywelivein@gmail.com>
2016-01-26 19:33:35 -0600

    Profile view is completely responsive

--- client/app/components/ProfileMatches.js
+++ client/app/components/ProfileMatches.js
@@ -6,7 +6,7 @@ class ProfileMatches extends Component {
     const { state, actions } = this.props
     return (
       <div className="profile-page-data">
-        <div>
+        <div className="profile-page-boxes">
           {this.props.state.profile.data.matches.map(match =>
             <ProfilePageMatchBox key={match.id} data={match} state={this.props.state} actions={this.props.actions} />
           )}
