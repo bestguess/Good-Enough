30b768e642311ce7d27628a49cdf17e8e9d1c1ed
Paolinni Velazquez <Paolinni@Paolinnis-MacBook-Pro.local>
2016-01-28 19:33:12 -0600

    added reset password page where the user is directed to from the email

--- client/app/components/NewPasswordForm.js
+++ client/app/components/NewPasswordForm.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 const { Link } = require('react-router');
 
-class UsernamePasswordResetForm extends Component {
+class NewPasswordForm extends Component {
   handleKeyUp(input) {
     console.log('input YO: ', this.refs[input].value)
     this.props.actions.saveRecoverPasswordInput(input, this.refs[input].value)
@@ -24,18 +24,19 @@ class UsernamePasswordResetForm extends Component {
     }
 
     if (this.props.state.usernamePasswordReset.userData.emailReceived) {
-      emailAlert = <span className="recover-password-email-success">An email has been sent to {this.props.state.usernamePasswordReset.userData.email} with further instructions</span>
+      emailAlert = <span className="recover-password-email-success">An email has been sent to {this.props.state.usernamePasswordReset.userData.email} with instructions to reset password</span>
     }
 
     var formButton = <button onClick={this.props.actions.recoverPassword} className="question-form-button">Submit</button>
     return (
       <div className="login-box">
-        <h1 className="login-logo password-reset">Password Recovery</h1>
+        <h1 className="login-logo password-reset">Reset your password</h1>
         <div className="login-form">
         {emailAlert}
         {loginErr}
         <div>
-          <input className="recover-password-email" placeholder="Enter Email" ref="email" onKeyUp={() => this.handleKeyUp('email')} onKeyPress={(event) => this.handleKeyPress(event)} />
+          <input className="recover-password-email" placeholder="Enter new password" ref="password" onKeyUp={() => this.handleKeyUp('email')} onKeyPress={(event) => this.handleKeyPress(event)} />
+          <input className="recover-password-email" placeholder="Confirm new password" ref="password" onKeyUp={() => this.handleKeyUp('email')} onKeyPress={(event) => this.handleKeyPress(event)} />
           </div>
         </div>
         <div className="question-form-submit-button-login">
@@ -46,9 +47,9 @@ class UsernamePasswordResetForm extends Component {
   }
 }
 
-UsernamePasswordResetForm.PropTypes = {
+NewPasswordForm.PropTypes = {
   state: PropTypes.object.isRequired,
   actions: PropTypes.object.isRequired
 }
 
-export default UsernamePasswordResetForm;
+export default NewPasswordForm;
