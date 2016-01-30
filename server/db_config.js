33bf5920fdd0586d5567797e880a0ed312d74720
ivanmora <moraivan1@gmail.com>
2016-01-30 09:35:25 -0600

    Removed unnecessary Auth Schema from db_config

--- server/db_config.js
+++ server/db_config.js
@@ -43,32 +43,6 @@ var questionsSchema = new Schema({
   skip: Boolean
 });
 
-var authSchema = new Schema({
-  user_id: String,
-  local: {
-    email: String,
-    password: String
-  },
-  facebook: {
-    id: String,
-    token: String,
-    email: String,
-    name: String
-  },
-  twitter: {
-    id: String,
-    token: String,
-    displayName: String,
-    username: String
-  },
-  google: {
-    id: String,
-    token: String,
-    email: String,
-    name: String
-  }
-});
-
 var tokenSchema = new Schema({
   user_id: String,
   token: String,
