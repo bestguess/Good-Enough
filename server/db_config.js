cbd3b5c1db793758bb638d4c0ef8924a5369f42f
ivanmora <moraivan1@gmail.com>
2016-01-30 09:36:57 -0600

    Removed items related to old auth schema

--- server/db_config.js
+++ server/db_config.js
@@ -49,25 +49,13 @@ var tokenSchema = new Schema({
   dateCreated: String
 });
 
-// Generates a hash
-authSchema.methods.generateHash = function(password){
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// Checks to see if the password is valid
-authSchema.methods.validPassword = function(password){
-  return bcrypt.compareSync(password, this.local.password);
-};
-
 var Users = mongoose.model('User', usersSchema);
 var Messages = mongoose.model('Message', messagesSchema);
 var Questions = mongoose.model('Questions', questionsSchema);
-var Auth = mongoose.model('Auth', authSchema);
 var Token = mongoose.model('Token', tokenSchema);
 
 module.exports.Users = Users;
 module.exports.Messages = Messages;
 module.exports.Questions = Questions;
-module.exports.Auth = Auth;
 module.exports.Token = Token;
 module.exports.db = db;
