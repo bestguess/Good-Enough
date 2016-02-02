6d399fcdb47521cc75929acb2d2a01fe19a4aece
Paolinni Velazquez <Paolinni@Paolinnis-MacBook-Pro.local>
2016-02-01 18:15:40 -0600

    submitNewPassword spinner now working

--- client/app/reducers/usernamePasswordReset.js
+++ client/app/reducers/usernamePasswordReset.js
@@ -1,4 +1,4 @@
-import { SAVE_RECOVER_PASSWORD_INPUT, RECOVER_PASSWORD, RECOVER_PASSWORD_FAILED, RESET_PASSWORD, RESET_PASSWORD_FAILED, SAVE_NEW_PASSWORD_INPUT, SAVE_CONFIRM_NEW_PASSWORD_INPUT, SUBMIT_NEW_PASSWORD, RECOVER_PASSWORD_IS_FETCHING } from '../constants/UsernamePasswordReset_ActionTypes'
+import { SAVE_RECOVER_PASSWORD_INPUT, RECOVER_PASSWORD, RECOVER_PASSWORD_FAILED, SAVE_NEW_PASSWORD_INPUT, SAVE_CONFIRM_NEW_PASSWORD_INPUT, SUBMIT_NEW_PASSWORD, SUBMIT_NEW_PASSWORD_FAILED, RECOVER_PASSWORD_IS_FETCHING } from '../constants/UsernamePasswordReset_ActionTypes'
 
 const initialState = {
   userData: {
@@ -7,7 +7,8 @@ const initialState = {
     foundEmail: true,
     emailReceived: false,
     newPassword: undefined,
-    confirmNewPassword: undefined
+    confirmNewPassword: undefined,
+    newPasswordReceived: false
   }
 }
 
@@ -42,24 +43,24 @@ export default function RecoverPassword(state = initialState, action) {
 
     case RECOVER_PASSWORD_FAILED:
       var newState = Object.assign({}, state)
-      newState.userData.foundEmail = false
+      newState.userData.foundEmail = false;
       newState.userData.isFetching = false;
-      return newState
-
-    case RESET_PASSWORD:
-      var newState = Object.assign({}, state)
-      return newState
-
-    case RESET_PASSWORD_FAILED:
-      var newState = Object.assign({}, state)
       return newState
 
     case SUBMIT_NEW_PASSWORD:
       var newState = Object.assign({}, state)
+      newState.userData.isFetching = false;
+      newState.userData.newPasswordReceived = true;
+      return newState
+
+    case SUBMIT_NEW_PASSWORD_FAILED:
+      var newState = Object.assign({}, state)
+      newState.userData.newPasswordReceived = false;
+      newState.userData.isFetching = false;
       return newState
 
     default:
       console.log('hit default case: returning state in RecoverPassword')
-      return {...state}
+      return state
   }
 }
