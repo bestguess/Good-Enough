102246a1bb9e67cbd80ab5e7cf9c5ad68c4c63fa
thecitywelivein <thecitywelivein@gmail.com>
2016-02-15 12:41:37 -0600

    Fixed login error

--- client/app/reducers/login.js
+++ client/app/reducers/login.js
@@ -1,4 +1,4 @@
-import { LOG_IN, SAVE_LOGIN_INPUT, LOG_IN_FAILED } from '../constants/Login_ActionTypes'
+import { LOG_IN, SAVE_LOGIN_INPUT, LOG_IN_FAILED, LOG_IN_RESTART } from '../constants/Login_ActionTypes'
 import { addAuthToken } from '../helpers'
 
 const initialState = {
@@ -20,19 +20,21 @@ export default function LogIn(state = initialState, action) {
       newState.userData[action.input] = action.value
       return newState
 
-
     case LOG_IN:
       var newState = Object.assign({}, state)
       addAuthToken(JSON.stringify(action.tokenData))
       newState.loggedStatus = true;
       return newState
 
-
     case LOG_IN_FAILED:
       var newState = Object.assign({}, state)
       newState.loggedStatus = false;
       return newState
 
+    case LOG_IN_RESTART:
+      var newState = Object.assign({}, state)
+      newState.loggedStatus = true;
+      return newState
 
     default:
     	return state
