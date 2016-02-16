5337c81507d050c2b9b5296ef9ea94507f916e41
thecitywelivein <thecitywelivein@gmail.com>
2016-02-16 13:09:11 -0600

    moved keys to outside file

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -4,7 +4,8 @@ var Token = db.Token;
 var im = require('imagemagick');
 var path = require('path');
 var AWS = require('aws-sdk');
-AWS.config.update({***REMOVED***});
+var goodies = require('../goodies.js');
+AWS.config.update(goodies.amazonConfig);
 
 module.exports = {
   convertPhoto : function(photo,email,callback){
