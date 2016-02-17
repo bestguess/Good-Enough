f311997367a68251bd23de649ff238422f2f2abd
Hank Couture <hankcouture@gmail.com>
2016-02-17 09:15:29 -0600

    hide back button on profile page for mobile

--- client/app/components/Nav/PrivateNav.js
+++ client/app/components/Nav/PrivateNav.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router'
 class PrivateNav extends Component {
   render() {
     const { state, actions } = this.props
+    var mobileBackButton;
+    if (window.location.pathname !== '/profile') mobileBackButton = <Link to="/profile" className="mobile-nav"><i className="fa fa-angle-double-left"></i></Link>
     return (
       <nav className="private-nav">
         <li className='nav-left'>
-          <Link to="/profile" className="mobile-nav"><i className="fa fa-angle-double-left"></i></Link>
+          {mobileBackButton}
           <Link to="/profile" className="desktop-nav">{this.props.state.profile.data.firstName} {this.props.state.profile.data.lastName}</Link>
         </li>
         <li className='nav-right'>
